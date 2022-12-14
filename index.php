772638f60c6dd736945b5440a662fe58f120f8ce
santhiswaroop2016 <31235961+santhiswaroop2016@users.noreply.github.com>
2022-12-14 18:48:45 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
-
-echo "Hello World!";
-echo "vivek";
+$name = "Quincy";
+echo "Hi! My name is " . $name . "<br>";
+echo "Hi! My name is " . $NAME . "<br>";
+echo "Hi! My name is " . $NaMe . "<br>";
+?>
