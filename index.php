1a45fb5e4b1ee4d9808b05a476559d5db941ae2f
santhiswaroop2016 <31235961+santhiswaroop2016@users.noreply.github.com>
2023-08-30 20:43:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-$name = "Harshini";
+$name = "Jitendra";
 echo "Hi! My name is " . $name . "<br>";
 echo "Hi! My name is " . $name . "<br>";
 echo "Hi! My name is " . $name . "<br>";
-$name1 = "SomaShekar";
+$name1 = "swaroop";
 echo "Hi! My name is " . $name1 . "<br>";
 echo "Hi! My name is " . $name1 . "<br>";
 echo "Hi! My name is " . $name1 . "<br>";
